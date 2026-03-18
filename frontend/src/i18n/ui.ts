573b8b0fbf57330d8bdabf7fa3d6ab1ecece78f4
Luis Alaguna <alagunasalahaddin@live.com>
2026-03-18 03:10:51 -0500

    refactor: split i18n messages to files

--- frontend/src/i18n/ui.ts
+++ frontend/src/i18n/ui.ts
@@ -1,3 +1,8 @@
+import { ar } from "./locales/ar";
+import { en } from "./locales/en";
+import { es } from "./locales/es";
+import { tr } from "./locales/tr";
+
 export const languages = {
   ar: "العربية",
   en: "English",
@@ -15,60 +20,8 @@ export const localeFlags = [
 export const defaultLang = "es";
 
 export const ui = {
-  en: {
-    "language-picker.label": "Select language",
-    "header.donate-now": "Donate",
-    "footer.contact": "Contact",
-    "footer.quick-links": "Quick Links",
-    "footer.newsletter": "Newsletter",
-    "footer.newsletter-description":
-      "Subscribe to our newsletter to get the latest updates.",
-    "footer.email-placeholder": "Enter your email",
-    "footer.subscribe": "Subscribe",
-    "theme-switcher.light-label": "Use system theme",
-    "theme-switcher.system-label": "Switch to dark theme",
-    "theme-switcher.dark-label": "Switch to light theme",
-  },
-  es: {
-    "language-picker.label": "Seleccionar idioma",
-    "header.donate-now": "Donar",
-    "footer.contact": "Contacto",
-    "footer.quick-links": "Enlaces rápidos",
-    "footer.newsletter": "Boletín",
-    "footer.newsletter-description":
-      "Suscríbete a nuestro boletín para recibir las últimas actualizaciones.",
-    "footer.email-placeholder": "Ingresa tu correo",
-    "footer.subscribe": "Suscribirse",
-    "theme-switcher.light-label": "Usar tema del sistema",
-    "theme-switcher.system-label": "Cambiar a tema oscuro",
-    "theme-switcher.dark-label": "Cambiar a tema claro",
-  },
-  tr: {
-    "language-picker.label": "Dil seç",
-    "header.donate-now": "Bağış yap",
-    "footer.contact": "İletişim",
-    "footer.quick-links": "Hızlı Bağlantılar",
-    "footer.newsletter": "Bülten",
-    "footer.newsletter-description":
-      "En son güncellemeleri almak için bültenimize abone olun.",
-    "footer.email-placeholder": "E-postanızı girin",
-    "footer.subscribe": "Abone ol",
-    "theme-switcher.light-label": "Sistem temasını kullan",
-    "theme-switcher.system-label": "Koyu temaya geç",
-    "theme-switcher.dark-label": "Açık temaya geç",
-  },
-  ar: {
-    "language-picker.label": "اختر اللغة",
-    "header.donate-now": "تبرع",
-    "footer.contact": "اتصل بنا",
-    "footer.quick-links": "روابط سريعة",
-    "footer.newsletter": "النشرة البريدية",
-    "footer.newsletter-description":
-      "اشترك في نشرتنا البريدية للحصول على آخر التحديثات.",
-    "footer.email-placeholder": "أدخل بريدك الإلكتروني",
-    "footer.subscribe": "اشترك",
-    "theme-switcher.light-label": "استخدم سمة النظام",
-    "theme-switcher.system-label": "بدّل إلى الوضع الداكن",
-    "theme-switcher.dark-label": "بدّل إلى الوضع الفاتح",
-  },
+  ar,
+  en,
+  es,
+  tr,
 } as const;
